7ee7843c9fbe00014b612fa3c4f92f22f05dda3f
Jaival <jaival.s.vyas@gmail.com>
2022-09-20 17:45:50 +0530

    added getter-setter method

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -6,4 +6,30 @@
 package com.niit.jdp.model;
 
 public class Playlist {
+    private String playlistName;
+    private Songs songs;
+
+    public Playlist() {
+    }
+
+    public Playlist(String playlistName, Songs songs) {
+        this.playlistName = playlistName;
+        this.songs = songs;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public Songs getSongs() {
+        return songs;
+    }
+
+    public void setSongs(Songs songs) {
+        this.songs = songs;
+    }
 }

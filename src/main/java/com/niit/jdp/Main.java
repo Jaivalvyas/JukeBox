4784de20198f2b52049e1358c12a41829b5317cd
Jaival <jaival.s.vyas@gmail.com>
2022-09-21 12:00:32 +0530

    Added database connection status

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,7 +1,15 @@
 package com.niit.jdp;
 
+import com.niit.jdp.service.DatabaseService;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("Hello world!");
+        DatabaseService databaseService = new DatabaseService();
+        databaseService.connect();
+        databaseService.getConnection();
+        databaseService.printConnectionStatus();
     }
 }

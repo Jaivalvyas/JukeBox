f148093ced04544c1c2465e626826662e5ceb820
Jaival <jaival.s.vyas@gmail.com>
2022-09-22 21:19:48 +0530

    created methods to perform display playlist

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -5,5 +5,92 @@
  */
 package com.niit.jdp.repository;
 
-public class PlaylistRepository {
+import com.niit.jdp.model.Playlist;
+import com.niit.jdp.model.Song;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlaylistRepository implements Repository<Playlist> {
+
+    @Override
+    public boolean add(Connection connection, Playlist playlist) throws SQLException {
+        Song song = new Song();
+        Playlist playlist1 = new Playlist();
+        playlist1.getSongList().get(song.getId());
+        String insertQuery = "INSERT INTO `jukebox`.`playlist` (`name`, `catalog_id`) VALUES ('?', '?');";
+        // create a statement object
+        int numberOfRowsAffected;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            // set the values of the query parameters
+            preparedStatement.setString(1, playlist.getPlaylistName());
+            preparedStatement.setInt(2, playlist1.getSongList().get(song.getId()));
+            // execute the query
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return numberOfRowsAffected > 0;
+    }
+
+    @Override
+    public List<Playlist> getAll(Connection connection) throws SQLException {
+
+        List<Playlist> songInPlaylist = new ArrayList<>();
+        Playlist playlist = new Playlist();
+        // create a SQL query to retrieve all the rows from the catalog table
+        String readQuery = "SELECT * FROM `jukebox`.`playlist`;";
+
+        // create a statement object
+        try (PreparedStatement preparedStatement = connection.prepareStatement
+                (readQuery, Statement.RETURN_GENERATED_KEYS
+                )) {
+
+            // execute the query
+            ResultSet songResultSet = preparedStatement.executeQuery();
+
+            // iterate over the result set and create a list of song objects
+            while (songResultSet.next()) {
+                // fetch the values of the current row from the result set
+                int id = songResultSet.getInt("id");
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                String name = songResultSet.getString("name");
+                String catalog = songResultSet.getString("catalog_id");
+                playlist.setPlaylistId(id);
+                playlist.setPlaylistName(name);
+
+                catalog = catalog.replaceAll("[\\[\\]]", "");
+                String[] catalogId = catalog.split(",");
+                List<Song> playlistArrayList = new ArrayList<>();
+                for (String songName : catalogId) {
+                    int index = Integer.parseInt(songName);
+                    SongRepository songRepository = new SongRepository();
+                    Song song = songRepository.getSongById(connection, index);
+                    playlistArrayList.add(song);
+//                    SongRepository songRepository = new SongRepository();
+//                    Song song = songRepository.getSongById(connection,id);
+//                    playlist.getSongList().add(song);
+                }
+                Playlist playlist1 = new Playlist(id, name, playlistArrayList);
+
+                songInPlaylist.add(playlist1);
+
+            }
+
+        }
+
+        //return the list of songs
+        return songInPlaylist;
+    }
+
+    @Override
+    public Playlist getSongById(Connection connection, int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Connection connection, int id) throws SQLException {
+        return false;
+    }
 }
--- src/main/java/com/niit/jdp/service/PlaylistService.java
+++ src/main/java/com/niit/jdp/service/PlaylistService.java
@@ -5,5 +5,83 @@
  */
 package com.niit.jdp.service;
 
+import com.niit.jdp.model.Playlist;
+import com.niit.jdp.model.Song;
+import com.niit.jdp.repository.PlaylistRepository;
+import com.niit.jdp.repository.SongRepository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class PlaylistService {
+    PlaylistRepository playlistRepository = new PlaylistRepository();
+
+    public List<Playlist> displayPlaylist() {
+        int choice = -1;
+        do {
+            System.out.println("============================================");
+            System.out.println("1. Add playlist");
+            System.out.println("2. View all playlist details");
+            System.out.println("3. Exit");
+            System.out.println("============================================");
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            DatabaseService databaseService = new DatabaseService();
+
+            try {
+                databaseService.connect();
+                PlaylistRepository playlistRepository = new PlaylistRepository();
+                Connection connection = databaseService.getConnection();
+
+                switch (choice) {
+                    case 1:
+                        Song song = new Song();
+                        System.out.println("Add a new playlist");
+                        System.out.println("Enter the name: ");
+                        String name = scanner.next();
+
+                        System.out.println("Enter song id you want to add in the playlist: ");
+                        int songId = scanner.nextInt();
+
+                        SongRepository songRepository = new SongRepository();
+                        Song song1 = songRepository.getSongById(connection, songId);
+
+                        List<Song> songList = new ArrayList<>();
+                        songList.add(song1);
+
+                        Playlist playlist = new Playlist(0, name, (List<Song>) songList.get(song1.getId()));
+                        playlistRepository.add(connection, playlist);
+                        break;
+
+                    case 2:
+                        System.out.println("View all playlist");
+                        playlistRepository.getAll(connection).forEach(System.out::println);
+                        break;
+
+                    case 3:
+                        System.out.println("Exit");
+                        break;
+                    default:
+                        System.err.println("Invalid choice");
+                }
+
+            } catch (ClassNotFoundException | SQLException exception) {
+                System.err.println("Could not connect to the database!");
+                exception.printStackTrace();
+                choice = 4;
+            }
+
+        } while (choice != 4);
+        return new ArrayList<>();
+    }
+
+
 }
+

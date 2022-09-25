98382c583c54041bee5b28e9852dad9b0546fb52
Jaival <jaival.s.vyas@gmail.com>
2022-09-25 23:55:03 +0530

    added songPath method

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -15,21 +15,15 @@ import java.util.List;
 public class PlaylistRepository implements Repository<Playlist> {
 
     @Override
-    public boolean add(Connection connection, Playlist playlist) throws SQLException {
+    public boolean addSongs(Connection connection, Playlist playlist) throws SQLException {
+        String insertCarQuery = "INSERT INTO `jukebox`.`playlist` (`name`, `catalog_id`) VALUES" + " (?, ?);";
         Song song = new Song();
-        Playlist playlist1 = new Playlist();
-        playlist1.getSongList().get(song.getId());
-        String insertQuery = "INSERT INTO `jukebox`.`playlist` (`name`, `catalog_id`) VALUES ('?', '?');";
-        // create a statement object
         int numberOfRowsAffected;
-        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-            // set the values of the query parameters
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertCarQuery)) {
             preparedStatement.setString(1, playlist.getPlaylistName());
-            preparedStatement.setInt(2, playlist1.getSongList().get(song.getId()));
-            // execute the query
+            preparedStatement.setString(2, String.valueOf(playlist.getSongList().get(song.getId()).getId()));
             numberOfRowsAffected = preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
         return numberOfRowsAffected > 0;
     }
@@ -43,9 +37,7 @@ public class PlaylistRepository implements Repository<Playlist> {
         String readQuery = "SELECT * FROM `jukebox`.`playlist`;";
 
         // create a statement object
-        try (PreparedStatement preparedStatement = connection.prepareStatement
-                (readQuery, Statement.RETURN_GENERATED_KEYS
-                )) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(readQuery, Statement.RETURN_GENERATED_KEYS)) {
 
             // execute the query
             ResultSet songResultSet = preparedStatement.executeQuery();
@@ -68,25 +60,71 @@ public class PlaylistRepository implements Repository<Playlist> {
                     SongRepository songRepository = new SongRepository();
                     Song song = songRepository.getSongById(connection, index);
                     playlistArrayList.add(song);
-//                    SongRepository songRepository = new SongRepository();
-//                    Song song = songRepository.getSongById(connection,id);
-//                    playlist.getSongList().add(song);
                 }
                 Playlist playlist1 = new Playlist(id, name, playlistArrayList);
-
                 songInPlaylist.add(playlist1);
-
             }
-
         }
-
         //return the list of songs
         return songInPlaylist;
     }
 
     @Override
     public Playlist getSongById(Connection connection, int id) throws SQLException {
-        return null;
+        Playlist playlist = new Playlist();
+        List<Playlist> songInPlaylist = new ArrayList<>();
+        String searchQuery = "SELECT * FROM `jukebox`.`playlist` WHERE(`id` = ?);";
+
+        Playlist playlist1 = null;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(searchQuery)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                // fetch the values of the current row from the result set
+                int playlistId = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String songsId = resultSet.getString("catalog_id");
+                playlist.setPlaylistId(id);
+                playlist.setPlaylistName(name);
+
+                songsId = songsId.replaceAll("[\\[\\]]", "");
+                String[] catalogId = songsId.split(",");
+                List<Song> playlistArrayList = new ArrayList<>();
+                for (String songName : catalogId) {
+                    int index = Integer.parseInt(songName);
+                    SongRepository songRepository = new SongRepository();
+                    Song song = songRepository.getSongById(connection, index);
+                    playlistArrayList.add(song);
+                }
+                playlist1 = new Playlist(playlistId, name, playlistArrayList);
+                // songInPlaylist.add(playlist1);
+            }
+        }
+        //return the list of songs
+        return playlist1;
+
+    }
+
+    public String getSongPath(Connection connection, int id) {
+        String seacrhPath = "SELECT `song_path` FROM `jukebox`.`catalog` WHERE (`id`=?);";
+
+        Song song = new Song();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(seacrhPath)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                // fetch the values of the current row from the result set
+                String songPath = resultSet.getString("song_path");
+                // create a student object using the values fetched from the result set
+                song = new Song(songPath);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return song.getSongPath();
     }
 
     @Override

3f75994e6ed693a52ff528e684e1e9f10180b468
Jaival <jaival.s.vyas@gmail.com>
2022-09-27 00:28:40 +0530

    added features like pause,resume,restart, stop in PlaySongService

--- src/main/java/com/niit/jdp/service/PlaySongService.java
+++ src/main/java/com/niit/jdp/service/PlaySongService.java
@@ -27,24 +27,51 @@ public class PlaySongService {
             // 7. start the sound file
             clip.start();
 
-            System.out.println("Select option");
-            System.out.println("1. Play in loop");
-            System.out.println("2. Stop song");
+            int flag = 0;
+            long clippause = 0;
 
-            int choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    // Play song in loop
-                    System.out.println("Enter no. of times you want to play this song in loop");
-                    int index = sc.nextInt();
-                    clip.loop(index);
-                    break;
-                case 2:
-                    clip.stop();
-                    System.out.println("Song stopped");
-                    break;
-                default:
-                    System.out.println("Invalid option");
+            while (flag == 0) {
+
+                System.out.println("Select option");
+                System.out.println("1. Play in loop");
+                System.out.println("2. Pause the song");
+                System.out.println("3. Resume the song");
+                System.out.println("4. Restart the song");
+                System.out.println("5. Stop");
+
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        // Play song in loop
+                        System.out.println("Enter no. of times you want to play this song in loop");
+                        int index = sc.nextInt();
+                        clip.loop(index);
+                        break;
+                    case 2:
+                        clippause = clip.getMicrosecondPosition();
+                        clip.stop();
+                        System.out.println("Song paused");
+                        break;
+                    case 3: {
+                        clip.setMicrosecondPosition(clippause);
+                        clip.start();
+                        System.out.println("Song Resume");
+                        break;
+                    }
+                    case 4: {
+                        clip.setMicrosecondPosition(0);
+                        clip.start();
+                        System.out.println("Song Restarted");
+                        break;
+                    }
+                    case 5: {
+                        clip.stop();
+                        flag = 0;
+                        break;
+                    }
+                    default:
+                        System.out.println("Invalid option");
+                }
             }
             // 8. pause the current thread for the time the song is being played
             long songDurationInMilliseconds = clip.getMicrosecondLength() / 1000L;
